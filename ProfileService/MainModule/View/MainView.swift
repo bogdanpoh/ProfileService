3ace51b980cedb62655aae5542e9f2c5436167bc
bogdanpoh <bogdan20205@gmail.com>
2021-01-18 17:48:29 +0200

    add views

--- ProfileService/MainModule/View/MainView.swift
+++ ProfileService/MainModule/View/MainView.swift
@@ -8,7 +8,58 @@
 import UIKit
 
 class MainView: UIView {
-
     
+    // MARK: - UI
+    
+    var avatarImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "avatar")
+        return imageView
+    }()
+    
+    var nickNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+// MARK: - Setup
+
+private extension MainView {
+    
+    func setupViews() {
+        addSubview(avatarImage)
+        addSubview(nickNameLabel)
+    }
+    
+    func configureConstraints() {
+        avatarImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: topAnchor),
+            avatarImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            avatarImage.widthAnchor.constraint(equalToConstant: 100),
+            avatarImage.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        nickNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nickNameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 20),
+            nickNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
 }
--- ProfileService/MainModule/View/MainViewController.swift
+++ ProfileService/MainModule/View/MainViewController.swift
@@ -9,14 +9,49 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - UI
+    
+    var mainView = MainView()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
+        configureConstraints()
     }
-
-
 }
 
+// MARK: - Setup
+
+private extension MainViewController {
+    
+    func setupViews() {
+        view.addSubview(mainView)
+    }
+    
+    func configureConstraints() {
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Set
+
+private extension MainViewController {
+    
+    func setNickName(text: String) {
+        mainView.nickNameLabel.text = text
+    }
+    
+    func setAvatar(image: UIImage) {
+        mainView.avatarImage.image = image
+    }
+}
