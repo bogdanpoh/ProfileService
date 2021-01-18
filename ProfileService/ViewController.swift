99a539cca7da6bc705ca21b4796a283cdd3ffd2a
bogdanpoh <bogdan20205@gmail.com>
2021-01-18 11:17:50 +0200

    Initial Commit

---
+++ ProfileService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileService
+//
+//  Created by Bogdan Pohidnya on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
